a429122835b694b90b9a293c4a0748fae0ce4f69
lopezsarmiento <diego.lopezsarmiento@gmail.com>
2020-07-09 15:28:57 -0300

    initial commit

---
+++ src/components/itemsList.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ItemsList = () => {
+  return (
+    <div className="container bg-light rounded-lg shadow-lg">
+      <div className="row">
+        <div className="col">
+          <h3 className="text-center mt-2">search results</h3>
+        </div>
+      </div>
+      <hr></hr>
+      <div className="row">
+        <div className="col"></div>
+      </div>
+    </div>
+  );
+};
+
+export default ItemsList;
