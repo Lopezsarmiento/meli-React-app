0a617d712de93b0174d1235465427dec796564c4
lopezsarmiento <diego.lopezsarmiento@gmail.com>
2020-07-21 00:42:06 -0300

    adding detail component

---
+++ src/components/common/thumbnail.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Thumbnail = ({ thumbnail }) => {
+  return <img src={thumbnail} className="card-img-top" alt="..." />;
+};
+
+export default Thumbnail;
--- src/components/detail.jsx
+++ src/components/detail.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 // components
 import Loading from "./loading";
+import Thumbnail from "./common/thumbnail";
 
 const Detail = ({ match }) => {
   const [itemInfo, setIteminfo] = useState({});
@@ -33,17 +34,13 @@ const Detail = ({ match }) => {
   console.log("iteminfo:", itemInfo);
 
   return (
-    <div className="container-fluid mt-2">
-      <div className="row">
-        {isLoading && <Loading></Loading>}
-        {!isLoading && (
-          <div className="col">
+    <div className="container-fluid mt-5">
+      {isLoading && <Loading></Loading>}
+      {!isLoading && (
+        <div className="row">
+          <div className="col-md-7">
             <div className="card mb-3">
-              <img
-                src={itemInfo.thumbnail}
-                className="card-img-top"
-                alt="..."
-              />
+              <Thumbnail thumbnail={itemInfo.thumbnail}></Thumbnail>
               <hr></hr>
               <div className="card-body">
                 <h5 className="card-title">{itemInfo.title}</h5>
@@ -58,8 +55,31 @@ const Detail = ({ match }) => {
               </div>
             </div>
           </div>
-        )}
-      </div>
+          <div className="col-md-5">
+            <div className="mb-3">
+              <div className="card-body">
+                <p className="card-text text-muted">
+                  {itemInfo.condition === "new" ? "nuevo" : "usado"}
+                  <span> - </span>
+                  {itemInfo.sold_quantity} vendidos
+                </p>
+                <h4 className="card-title">{itemInfo.title}</h4>
+                <h1 className="card-text">${itemInfo.price}</h1>
+                {itemInfo.available_quantity > 0 && (
+                  <p className="card-text">Stock disponible</p>
+                )}
+                <p className="badge badge-primary">
+                  <span className="card-text">Envío con normalidad</span>
+                </p>
+
+                <p className="card-text">
+                  <small className="text-muted">Last updated 3 mins ago</small>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
